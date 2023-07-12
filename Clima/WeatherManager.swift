ade1bcf8e3bf9cca7c52caa96a84760a30c03e5f
Badal Aryal <badal.aryal@gmail.com>
2023-07-12 17:54:40 +0545

    Just added Kathmandu to the design

---
+++ Clima/WeatherManager.swift
@@ -0,0 +1,13 @@
+//
+//  WeatherManager.swift
+//  Clima
+//
+//  Created by Badal  Aryal on 12/07/2023.
+//  Copyright © 2023 App Brewery. All rights reserved.
+//
+
+import Foundation
+
+struct WeatherManager{
+    
+}
